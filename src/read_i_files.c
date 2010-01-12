8cdb48414464a8233668b1f5226086fccc308b95
zac <zac@cdf9f54e-c1c6-40d2-88f3-cdd711570eaa>
2010-01-12 17:04:45 +0000

    In the function get_cmtf of read_i_files.c : The first 5 characters of the PDELINE are now not important. Before this revision (r111), it has to be either:        " PDE "        " MLI "        " ISC "        " REB "        " SWE "        " HSW "        " JMA "        " PDEW"        " SWEQ"
    git-svn-id: file:///home/svn/svn/wphase_project/trunk@111 cdf9f54e-c1c6-40d2-88f3-cdd711570eaa

--- src/read_i_files.c
+++ src/read_i_files.c
@@ -46,8 +46,8 @@ get_cmtf(eq, flag)
   FILE   *cmtfile       ;
   
   /* Allocate memory    */
-  line = char_alloc(LSIZE);
-  
+  line = char_alloc(LSIZE);  
+
   /* Openning file      */
   cmtfile = openfile_rt(eq->cmtfile,&nl);
   
@@ -75,119 +75,129 @@ get_cmtf(eq, flag)
       exit(1) ; 
     } 
 
-  /* Read lines */
-  nl = 0;
-  while( fgets(line,LSIZE,cmtfile) != NULL )
+  /* Read PDELINE */
+  nl = 1;
+  if (fgets(eq->pdeline,LSIZE,cmtfile) == NULL)
     {
-      nb = nb_blank(line) ;
-      if (nb != strlen(line))
-	nb2 = nb;
-      else
-	continue ;
-      if (strncmp(&line[nb2],"PDE",3)==0 || 
-	  strncmp(&line[nb2],"MLI",3)==0 || 
-	  strncmp(&line[nb2],"ISC",3)==0 || 
-	  strncmp(&line[nb2],"REB",3)==0 || 
-	  strncmp(&line[nb2],"SWE",3)==0 || 
-	  strncmp(&line[nb2],"HSW",3)==0 || 
-	  strncmp(&line[nb2],"JMA",3)==0)
+      fprintf(stderr,"ERROR: reading pdeline in cmtfile %s\n",eq->cmtfile) ;
+      exit(1);
+    }
+  tmp1 = sscanf(&eq->pdeline[5], "%i %i %i %i %i %lf %lf %lf %lf", 
+		&eq->ot_ye, &eq->ot_mo, &eq->ot_dm, &eq->ot_ho, &eq->ot_mi, 
+		&tmp2     , &eq->pde_evla, &eq->pde_evlo, &eq->pde_evdp ) ;
+  check_scan(9, tmp1   , eq->cmtfile  , cmtfile);
+  eq->ot_se = (int) tmp2;
+  eq->ot_ms = (int) ((tmp2 - eq->ot_se)*1000 + 0.5);
+ 
+  /* Read the rest of CMTFILE */
+  if (flag == 1 || flag == 2)
+    {
+      while( fgets(line,LSIZE,cmtfile) != NULL )
 	{
-	  strcpy(eq->pdeline,line);
-	  if (strncmp(&line[nb2],"PDEW",4) || strncmp(&line[nb2],"SWEQ",4))
-	    nb2+= 4 ;
-	  else if (line[nb2+3] == ' ')
-	    nb2+= 3 ;
-	  else {
-	    fprintf(stderr,"ERROR reading PDE in %s (unknown format) \n", eq->cmtfile) ;
-	    fclose(cmtfile) ;
-	    exit(1)         ; }  
-	  tmp1 = sscanf(&line[nb2], "%i %i %i %i %i %lf %lf %lf %lf", 
-			&eq->ot_ye, &eq->ot_mo, &eq->ot_dm, 
-			&eq->ot_ho, &eq->ot_mi, &tmp2     , 
-			&eq->pde_evla , &eq->pde_evlo , &eq->pde_evdp ) ;
-	  check_scan(9, tmp1   , eq->cmtfile  , cmtfile);
-	  eq->ot_se = (int) tmp2;
-	  eq->ot_ms = (int) ((tmp2 - eq->ot_se)*1000 + 0.5);
-	  nl++;	
-	}
-      else if (flag == 1 || flag == 2)
-	{
-	  if (strncmp(&line[nb2],"event name",10)==0){
-	    nb2+= 10+nb_blank(line+10)+1 ;
-	    tmp1 = sscanf (&line[nb2], "%s", eq->evid) ;
-	    check_scan(1, tmp1, eq->cmtfile, cmtfile) ;
-	    nl++; }
-	  else if (strncmp(&line[nb2],"time shift",10)==0){
-	    nb2+= 10+nb_blank(line+10)+1 ;
-	    tmp1 = sscanf (&line[nb2], "%lf", &eq->ts) ;
-	    check_scan(1, tmp1, eq->cmtfile, cmtfile) ;
-	    nl++; }
-	  else if (strncmp(&line[nb2],"half duration",13)==0){
-	    nb2+= 13+nb_blank(line+13)+1 ;
-	    tmp1 = sscanf (&line[nb2], "%lf", &eq->hd) ;
-	    check_scan(1, tmp1, eq->cmtfile, cmtfile) ;
-	    nl++; }
-	  else if (strncmp(&line[nb2],"latitude",8)==0){
-	    nb2+= 8+nb_blank(line+8)+1 ;
-	    tmp1 = sscanf (&line[nb2], "%lf\n", &eq->evla) ;
-	    check_scan(1, tmp1, eq->cmtfile, cmtfile)    ;
-	    nl++; }     
-	  else if (strncmp(&line[nb2],"longitude",9)==0){
-	    nb2+= 9+nb_blank(line+9)+1 ;
-	    tmp1 = sscanf (&line[nb2], "%lf\n", &eq->evlo) ;
-	    check_scan(1, tmp1, eq->cmtfile, cmtfile)    ;
-	    nl++; }     
-	  else if (strncmp(&line[nb2],"depth",5)==0){
-	    nb2+= 5+nb_blank(line+5)+1 ;
-	    tmp1 = sscanf (&line[nb2], "%lf", &eq->evdp) ;
-	    check_scan(1, tmp1, eq->cmtfile, cmtfile)    ;
-	    nl++; }
-
+	  nb = nb_blank(line) ;
+	  if (nb != strlen(line))
+	    nb2 = nb;
+	  else
+	    continue ;
+	  if (strncmp(&line[nb2],"event name",10)==0)
+	    {
+	      nb2+= 10+nb_blank(line+10)+1 ;
+	      tmp1 = sscanf (&line[nb2], "%s", eq->evid) ;
+	      check_scan(1, tmp1, eq->cmtfile, cmtfile) ;
+	      nl++; 
+	    }
+	  else if (strncmp(&line[nb2],"time shift",10)==0)
+	    {
+	      nb2+= 10+nb_blank(line+10)+1 ;
+	      tmp1 = sscanf (&line[nb2], "%lf", &eq->ts) ;
+	      check_scan(1, tmp1, eq->cmtfile, cmtfile) ;
+	      nl++; 
+	    }
+	  else if (strncmp(&line[nb2],"half duration",13)==0)
+	    {
+	      nb2+= 13+nb_blank(line+13)+1 ;
+	      tmp1 = sscanf (&line[nb2], "%lf", &eq->hd) ;
+	      check_scan(1, tmp1, eq->cmtfile, cmtfile) ;
+	      nl++; 
+	    }
+	  else if (strncmp(&line[nb2],"latitude",8)==0)
+	    {
+	      nb2+= 8+nb_blank(line+8)+1 ;
+	      tmp1 = sscanf (&line[nb2], "%lf\n", &eq->evla) ;
+	      check_scan(1, tmp1, eq->cmtfile, cmtfile)    ;
+	      nl++; 
+	    }     
+	  else if (strncmp(&line[nb2],"longitude",9)==0)
+	    {
+	      nb2+= 9+nb_blank(line+9)+1 ;
+	      tmp1 = sscanf (&line[nb2], "%lf\n", &eq->evlo) ;
+	      check_scan(1, tmp1, eq->cmtfile, cmtfile)    ;
+	      nl++; 
+	    }     
+	  else if (strncmp(&line[nb2],"depth",5)==0)
+	    {
+	      nb2+= 5+nb_blank(line+5)+1 ;
+	      tmp1 = sscanf (&line[nb2], "%lf", &eq->evdp) ;
+	      check_scan(1, tmp1, eq->cmtfile, cmtfile)    ;
+	      nl++; 
+	    }
 	  if (flag == 2)
 	    {
-	      if (strncmp(&line[nb2],"Mrr",3)==0){
-		nb2    += 3+nb_blank(line+3)+1 ;
-		tmp1    = sscanf (&line[nb2], "%lf", &eq->vm[1][0]) ;
-		eq->vm[1][0] /= (double)POW;
-		check_scan(1, tmp1, eq->cmtfile, cmtfile) ;
-		nl++;    }
-	      else if (strncmp(&line[nb2],"Mtt",3)==0){
-		nb2    += 3+nb_blank(line+3)+1 ;
-		tmp1    = sscanf (&line[nb2], "%lf", &eq->vm[1][1]) ;
-		eq->vm[1][1] /= (double)POW;
-		check_scan(1, tmp1, eq->cmtfile, cmtfile) ;
-		nl++;    }
-	      else if (strncmp(&line[nb2],"Mpp",3)==0){
-		nb2    += 3+nb_blank(line+3)+1 ;
-		tmp1    = sscanf (&line[nb2], "%lf", &eq->vm[1][2]) ;
-		eq->vm[1][2] /= (double)POW;
-		check_scan(1, tmp1, eq->cmtfile, cmtfile) ;
-		nl++;    }
-	      else if (strncmp(&line[nb2],"Mrt",3)==0){
-		nb2    += 3+nb_blank(line+3)+1 ;
-		tmp1    = sscanf (&line[nb2], "%lf", &eq->vm[1][3]) ;
-		eq->vm[1][3] /= (double)POW;
-		check_scan(1, tmp1, eq->cmtfile, cmtfile) ;
-		nl++;    }
-	      else if (strncmp(&line[nb2],"Mrp",3)==0){
-		nb2    += 3+nb_blank(line+3)+1 ;
-		tmp1    = sscanf (&line[nb2], "%lf", &eq->vm[1][4]) ;
-		eq->vm[1][4] /= (double)POW;	  
-		check_scan(1, tmp1, eq->cmtfile, cmtfile) ;
-		nl++;    }      
-	      else if (strncmp(&line[nb2],"Mtp",3)==0){
-		nb2    += 3+nb_blank(line+3)+1 ;
-		tmp1    = sscanf (&line[nb2], "%lf", &eq->vm[1][5]) ;
-		eq->vm[1][5] /= (double)POW;
-		check_scan(1, tmp1, eq->cmtfile, cmtfile) ;
-		nl++;    }
+	      if (strncmp(&line[nb2],"Mrr",3)==0)
+		{
+		  nb2    += 3+nb_blank(line+3)+1 ;
+		  tmp1    = sscanf (&line[nb2], "%lf", &eq->vm[1][0]) ;
+		  eq->vm[1][0] /= (double)POW;
+		  check_scan(1, tmp1, eq->cmtfile, cmtfile) ;
+		  nl++;   
+		}
+	      else if (strncmp(&line[nb2],"Mtt",3)==0)
+		{
+		  nb2    += 3+nb_blank(line+3)+1 ;
+		  tmp1    = sscanf (&line[nb2], "%lf", &eq->vm[1][1]) ;
+		  eq->vm[1][1] /= (double)POW;
+		  check_scan(1, tmp1, eq->cmtfile, cmtfile) ;
+		  nl++;   
+		}
+	      else if (strncmp(&line[nb2],"Mpp",3)==0)
+		{
+		  nb2    += 3+nb_blank(line+3)+1 ;
+		  tmp1    = sscanf (&line[nb2], "%lf", &eq->vm[1][2]) ;
+		  eq->vm[1][2] /= (double)POW;
+		  check_scan(1, tmp1, eq->cmtfile, cmtfile) ;
+		  nl++;    
+		}
+	      else if (strncmp(&line[nb2],"Mrt",3)==0)
+		{
+		  nb2    += 3+nb_blank(line+3)+1 ;
+		  tmp1    = sscanf (&line[nb2], "%lf", &eq->vm[1][3]) ;
+		  eq->vm[1][3] /= (double)POW;
+		  check_scan(1, tmp1, eq->cmtfile, cmtfile) ;
+		  nl++;    
+		}
+	      else if (strncmp(&line[nb2],"Mrp",3)==0)
+		{
+		  nb2    += 3+nb_blank(line+3)+1 ;
+		  tmp1    = sscanf (&line[nb2], "%lf", &eq->vm[1][4]) ;
+		  eq->vm[1][4] /= (double)POW;	  
+		  check_scan(1, tmp1, eq->cmtfile, cmtfile) ;
+		  nl++;    
+		}      
+	      else if (strncmp(&line[nb2],"Mtp",3)==0)
+		{
+		  nb2    += 3+nb_blank(line+3)+1 ;
+		  tmp1    = sscanf (&line[nb2], "%lf", &eq->vm[1][5]) ;
+		  eq->vm[1][5] /= (double)POW;
+		  check_scan(1, tmp1, eq->cmtfile, cmtfile) ;
+		  nl++;    
+		}
 	    }
 	}
     }
   fclose(cmtfile);
 
-  if (flag == 1){
-    strcpy(eq->cmtfile, "NO REF. SOL. USED") ;}  
+  if (flag == 1)
+    strcpy(eq->cmtfile, "NO REF. SOL. USED") ;
 
   /* Memory Freeing */
   free((void *)line) ;  
@@ -347,86 +357,118 @@ get_i_master(file, keys, n, eq)
       for (i=0 ; i<n ; i++) /* Keyword search */
 	if (strncmp(&line[nb2],keys[i],strlen(keys[i]))==0) 
 	  {
-	    if (strncmp(&line[nb2],"EVNAME",6)==0) {
-	      nb2+= 6+nb_blank(&line[nb2+6])+1          ;
-	      tmp = sscanf (&line[nb2], "%s", eq->evnm) ;
-	      check_scan(1, tmp, file, i_file)          ;
-	       nl++;   }
-	    else if (strncmp(&line[nb2],"SEED",4)==0) {
-	      nb2+= 4+nb_blank(&line[nb2+4])+1          ;
-	      tmp = sscanf (&line[nb2], "%s", eq->seed) ;
-	      check_scan(1, tmp, file, i_file)          ;
-	       nl++;   }
-	    else if (strncmp(&line[nb2],"CMTFILE",7)==0){
-	      nb2+= 7+nb_blank(&line[nb2+7])+1               ;
-	      if (strlen(eq->cmtfile) == 0){
-		tmp = sscanf (&line[nb2], "%s", eq->cmtfile) ;
-		check_scan(1, tmp, file, i_file)             ;}
-	      nl++;   }
-	    else if (strncmp(&line[nb2],"DMIN",4)==0){
-	      nb2+= 4+nb_blank(&line[nb2+4])+1            ;
-	      tmp = sscanf (&line[nb2], "%lf", &eq->dmin) ;
-	      check_scan(1, tmp, file, i_file)            ;}
-	    else if (strncmp(&line[nb2],"DMAX",4)==0){
-	      nb2+= 4+nb_blank(&line[nb2+4])+1              ;
-	      tmp = sscanf ((&line[nb2]), "%lf", &eq->dmax) ;
-	      check_scan(1, tmp, file, i_file)              ;
-	      nl++ ;  }
-	    else if (strncmp(&line[nb2],"filt_order",10)==0){
-	      nb2+= 10+nb_blank(&line[nb2+10])+1                ;
-	      tmp = sscanf ((&line[nb2]), "%d", &eq->filtorder) ;
-	      check_scan(1, tmp, file, i_file)                  ;
-	      nl++ ;  }
-	    else if (strncmp(&line[nb2],"filt_pass",9)==0){
-	      nb2+= 9+nb_blank(&line[nb2+9])+1                  ;
-	      tmp = sscanf ((&line[nb2]), "%d", &eq->filtnpass) ;
-	      check_scan(1, tmp, file, i_file)                  ;
-	      nl++ ;  }
-	    else if (strncmp(&line[nb2],"filt_cf1",8)==0){
-	      nb2+= 8+nb_blank(&line[nb2+8])+1              ;
-	      tmp = sscanf ((&line[nb2]), "%lf", &eq->flow) ;
-	      check_scan(1, tmp, file, i_file)              ;
-	      nl++ ;  }
-	    else if (strncmp(&line[nb2],"filt_cf2",8)==0){
-	      nb2+= 8+nb_blank(&line[nb2+8])+1               ;
-	      tmp = sscanf ((&line[nb2]), "%lf", &eq->fhigh) ;
-	      check_scan(1, tmp, file, i_file)               ;
-	      nl++ ;  }
-	    else if (strncmp(&line[nb2],"IDEC_2",6)==0){
-	      nb2+= 6+nb_blank(&line[nb2+6])+1           ;
-	      tmp = sscanf (&line[nb2], "%lf %lf %lf", 
-			    &eq->idtr, &eq->preevent, &eq->fend) ;
-	      check_scan(3, tmp, file, i_file)           ;
-	      nl++;   }
-	    else if (strncmp(&line[nb2],"IDEC_3",6)==0) {
-	      nb2+= 6+nb_blank(&line[nb2+6])+1            ;
-	      tmp = sscanf (&line[nb2], "%lf %lf %d %lf", 
+	    if (strncmp(&line[nb2],"EVNAME",6)==0) 
+	      {
+		nb2+= 6+nb_blank(&line[nb2+6])+1          ;
+		tmp = sscanf (&line[nb2], "%s", eq->evnm) ;
+		check_scan(1, tmp, file, i_file)          ;
+		nl++;   
+	      }
+	    else if (strncmp(&line[nb2],"SEED",4)==0) 
+	      {
+		nb2+= 4+nb_blank(&line[nb2+4])+1          ;
+		tmp = sscanf (&line[nb2], "%s", eq->seed) ;
+		check_scan(1, tmp, file, i_file)          ;
+		nl++;   
+	      }
+	    else if (strncmp(&line[nb2],"CMTFILE",7)==0)
+	      {
+		nb2+= 7+nb_blank(&line[nb2+7])+1               ;
+		if (strlen(eq->cmtfile) == 0)
+		  {
+		    tmp = sscanf (&line[nb2], "%s", eq->cmtfile) ;
+		    check_scan(1, tmp, file, i_file)             ;
+		  }
+		nl++;   
+	      }
+	    else if (strncmp(&line[nb2],"DMIN",4)==0)
+	      {
+		nb2+= 4+nb_blank(&line[nb2+4])+1            ;
+		tmp = sscanf (&line[nb2], "%lf", &eq->dmin) ;
+		check_scan(1, tmp, file, i_file)            ;
+	      }
+	    else if (strncmp(&line[nb2],"DMAX",4)==0)
+	      {
+		nb2+= 4+nb_blank(&line[nb2+4])+1              ;
+		tmp = sscanf ((&line[nb2]), "%lf", &eq->dmax) ;
+		check_scan(1, tmp, file, i_file)              ;
+		nl++ ;  
+	      }
+	    else if (strncmp(&line[nb2],"filt_order",10)==0)
+	      {
+		nb2+= 10+nb_blank(&line[nb2+10])+1                ;
+		tmp = sscanf ((&line[nb2]), "%d", &eq->filtorder) ;
+		check_scan(1, tmp, file, i_file)                  ;
+		nl++ ;  
+	      }
+	    else if (strncmp(&line[nb2],"filt_pass",9)==0)
+	      {
+		nb2+= 9+nb_blank(&line[nb2+9])+1                  ;
+		tmp = sscanf ((&line[nb2]), "%d", &eq->filtnpass) ;
+		check_scan(1, tmp, file, i_file)                  ;
+		nl++ ;  
+	      }
+	    else if (strncmp(&line[nb2],"filt_cf1",8)==0)
+	      {
+		nb2+= 8+nb_blank(&line[nb2+8])+1              ;
+		tmp = sscanf ((&line[nb2]), "%lf", &eq->flow) ;
+		check_scan(1, tmp, file, i_file)              ;
+		nl++ ;  
+	      }
+	    else if (strncmp(&line[nb2],"filt_cf2",8)==0)
+	      {
+		nb2+= 8+nb_blank(&line[nb2+8])+1               ;
+		tmp = sscanf ((&line[nb2]), "%lf", &eq->fhigh) ;
+		check_scan(1, tmp, file, i_file)               ;
+		nl++ ;  
+	      }
+	    else if (strncmp(&line[nb2],"IDEC_2",6)==0)
+	      {
+		nb2+= 6+nb_blank(&line[nb2+6])+1           ;
+		tmp = sscanf (&line[nb2], "%lf %lf %lf", 
+			      &eq->idtr, &eq->preevent, &eq->fend) ;
+		check_scan(3, tmp, file, i_file)           ;
+		nl++;   
+	      }
+	    else if (strncmp(&line[nb2],"IDEC_3",6)==0) 
+	      {
+		nb2+= 6+nb_blank(&line[nb2+6])+1            ;
+		tmp = sscanf (&line[nb2], "%lf %lf %d %lf", 
 			      &eq->fl, &eq->fh, 
-			    &eq->nf, &eq->tol)            ;
-	      check_scan(4, tmp, file, i_file)            ;
-	      nl++;   }
-	    else if (strncmp(&line[nb2],"GFDIR",5)==0) {
-	      nb2+= 5+nb_blank(&line[nb2+5])+1            ;
-	      tmp = sscanf (&line[nb2], "%s", eq->gf_dir) ;
-	      check_scan(1, tmp, file, i_file)            ;
-	      add_slash(eq->gf_dir);}
+			      &eq->nf, &eq->tol)            ;
+		check_scan(4, tmp, file, i_file)            ;
+		nl++;   
+	      }
+	    else if (strncmp(&line[nb2],"GFDIR",5)==0) 
+	      {
+		nb2+= 5+nb_blank(&line[nb2+5])+1            ;
+		tmp = sscanf (&line[nb2], "%s", eq->gf_dir) ;
+		check_scan(1, tmp, file, i_file)            ;
+		add_slash(eq->gf_dir);
+	      }
 	      
-	    else if (strncmp(&line[nb2],"WP_WIN",6)==0) {
-	      nb2+= 6+nb_blank(&line[nb2+6])+1       ;
-	      decode_wp_win(&line[nb2], eq->wp_win4) ;
-	      nl++;   }
+	    else if (strncmp(&line[nb2],"WP_WIN",6)==0) 
+	      {
+		nb2+= 6+nb_blank(&line[nb2+6])+1       ;
+		decode_wp_win(&line[nb2], eq->wp_win4) ;
+		nl++;   
+	      }
 	  }
     }
 
   fclose(i_file)     ;
 
   /* Check nl */
-  if (n > nl)  {
-    fprintf(stderr,"ERROR 2 : incomplete i_master file : %s\n", file) ;
-    exit(1) ; }
-  else if (n < nl) {
-    fprintf(stderr,"ERROR : redundancy in i_master file : %s\n", file) ;
-    exit(1) ; }
+  if (n > nl)  
+    {
+      fprintf(stderr,"ERROR 2 : incomplete i_master file : %s\n", file) ;
+      exit(1) ; 
+    }
+  else if (n < nl) 
+    {
+      fprintf(stderr,"ERROR : redundancy in i_master file : %s\n", file) ;
+      exit(1) ; 
+    }
   /* Memory Freeing */
   free((void *)buf)  ;
   free((void *)line) ;
