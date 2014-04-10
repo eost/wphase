e8b023e6c4eb9382dba85d1428c60136737d96d9
Luis Rivera <luis.rivera@unistra.fr>
2014-04-10 08:34:25 -0700

    Correction numero de version

--- bin/wp_grid_search.py
+++ bin/wp_grid_search.py
@@ -70,7 +70,7 @@ print 'GF_PATH is %s'%(GF_PATH)
 #entfile = WPHOME+'.svn/entries'
 #if os.path.exists(entfile):
 #	VERSION += open(entfile).readlines()[3].strip()
-VERSION = 'Version: r246'
+VERSION = 'Version: r248'
 
 BIN = WPHOME+'bin/'
 
