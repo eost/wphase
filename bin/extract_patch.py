7f65aa62b6418b1d8411ec939ea56f5c5f87253a
Zacharie Duputel <zacharie.duputel@unistra.fr>
2015-02-02 18:21:30 +0100

    Adding License header in bin/Arguments.py bin/extract_patch.py bin/sacpy.py bin/utils.py

--- bin/extract_patch.py
+++ bin/extract_patch.py
@@ -1,6 +1,35 @@
 #!/usr/bin/env python
 # -- coding: iso-8859-1 --
 
+############################################################################
+#
+#                  W phase source inversion package                 
+#                               -------------
+#
+#        Main authors: Zacharie Duputel, Luis Rivera and Hiroo Kanamori
+#                      
+# (c) California Institute of Technology and Universite de Strasbourg / CNRS 
+#                                  April 2013
+#
+#    Neither the name of the California Institute of Technology (Caltech) 
+#    nor the names of its contributors may be used to endorse or promote 
+#    products derived from this software without specific prior written 
+#    permission
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+############################################################################
 
 from sys        import stdin, stdout, stderr
 from os.path    import exists, isdir
--- bin/utils.py
+++ bin/utils.py
@@ -4,6 +4,36 @@ My Utils
 Written by Z. Duputel, September 2013
 '''
 
+############################################################################
+#
+#                  W phase source inversion package                 
+#                               -------------
+#
+#        Main authors: Zacharie Duputel, Luis Rivera and Hiroo Kanamori
+#                      
+# (c) California Institute of Technology and Universite de Strasbourg / CNRS 
+#                                  April 2013
+#
+#    Neither the name of the California Institute of Technology (Caltech) 
+#    nor the names of its contributors may be used to endorse or promote 
+#    products derived from this software without specific prior written 
+#    permission
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+############################################################################
+
 # Externals
 from os.path import exists,isdir
 from numpy   import ndarray
